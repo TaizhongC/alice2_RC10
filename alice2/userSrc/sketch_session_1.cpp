980bf10742f47d21289896b98cc9ee1440c54c7b
Taizhong Chen <Taizhong.Chen@zaha-hadid.com>
2025-11-06 18:07:06 +0000

    sending session 1 file

--- alice2/userSrc/sketch_session_1.cpp
+++ alice2/userSrc/sketch_session_1.cpp
@@ -7,6 +7,32 @@
 #include <sketches/SketchRegistry.h>
 
 using namespace alice2;
+using namespace std;
+
+int myInt;
+float myFloat;
+string myString;
+int myInt2;
+
+class Point{
+    float x;
+    float y;
+    float z;
+};
+
+class Line{
+    Point a;
+    Point b;
+};
+
+vector<Vec3> points;
+int num = 20;
+int numRows = 10;
+int numCols = 10;
+
+Vec3 minBB(0,0,0);
+Vec3 maxBB (10,10,0);
+
 
 class Session_1_Sketch : public ISketch {
 public:
@@ -17,17 +43,70 @@ public:
     void setup() override {
         // Initialize your sketch here
         // This is called once when the sketch is loaded
+
+        // The first line of code we did
+        // printf("\nhello world\n");
+        // cout << "hello world" << myInt << endl;
+
+        // manual push back a lot of points
+        // Vec3 pt(0, 0, 0);
+        // Vec3 pt_1(0, 0, 0);
+        // points.push_back(pt);
+        // points.push_back(pt_1);
+
+        // adding a row of points in x direction
+        // for(int i = 0; i < num; i++)
+        // {
+        //     // in every iteration, we make a new point, add to the container "points"
+        //     Vec3 myPoint(i, 0, 0);
+        //     points.push_back(myPoint);
+        // }
+
+        // we use row major to create a 2d field points
+        for(int i = 0; i < numCols; i++)
+        {
+            for(int j = 0; j < numRows; j++)
+            {
+                float x = minBB.x + i * (maxBB.x - minBB.x) / numRows;
+                float y = minBB.y + j * (maxBB.y - minBB.y) / numCols;
+                points.emplace_back(x, y, 0.0f);
+            }
+        }
     }
 
     void update(float deltaTime) override {
         // Update your sketch logic here
         // This is called every frame
         // deltaTime is the time elapsed since the last frame in seconds
+
+        // printf("\nhello world\n");
     }
 
     void draw(Renderer& renderer, Camera& camera) override {
         // Draw your custom content here
         // This is called every frame after update()
+
+        // make a point and draw
+        // Vec3 pt(0, 0, 0);
+        // Color col(1, 1, 1);
+        // renderer.drawPoint(pt, col, 10);
+
+        // Vec3 pt_1(1, 0, 0);
+        // Color col_1(1, 0, 0);
+        // renderer.drawPoint(pt_1, col_1, 10);
+        // renderer.drawPoint(Vec3(0,0,0));
+
+        // for(int i = 0; i < points.size(); i++)
+        // {
+        //     // iterate the container, draw the elements
+        //     renderer.drawPoint(points[i], Color(1, 1, 1), 10);
+        // }
+
+        for(int i = 0; i < points.size(); i++)
+        {
+            renderer.drawPoint(points[i], Color(1, 1, 1), 10);
+        }
+
     }
 
     void cleanup() override {
